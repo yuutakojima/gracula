5de7c00a4b72d7841c9f2e3401efe5f922e18b25
yu <>
2020-07-28 22:55:06 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="gracula",
+    version="1.0",
+    author="yutakojima",
+    author_email="info@idea-tools.space",
+    description="very simple twitter api",
+    long_description="very simple twitter api. This package can be done without OAuth.",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
