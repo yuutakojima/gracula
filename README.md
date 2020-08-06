feb25ad0fd8bcba86dae07ef831927882500ea3e
yu <>
2020-08-06 23:07:19 +0900

    fix readme and setup

--- README.md
+++ README.md
@@ -1,27 +1,9 @@
+# Introduction
 
-# Description
-very simple twitter api. This package can be done without OAuth.
+Gracula is a very simple Twitter api.
+
+You can use Twitter api without installing any other pip.
+
+This package can be done without OAuth.
+
 It consists of the standard library of python3.
-
-# usage
-
-```python
-import gracula
-
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_TOKEN = ''
-ACCESS_SECRET = ''
-
-g = gracula.Api(
-    consumer_key=CONSUMER_KEY,
-    consumer_secret=CONSUMER_SECRET,
-    access_token=ACCESS_TOKEN,
-    access_secret_key=ACCESS_SECRET
-)
-
-items = g.get_follow_user_list()
-
-for item in items['users']:
-    print(item['screen_name'])
-```
--- setup.py
+++ setup.py
@@ -4,21 +4,20 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open('README.md') as f:
     long_description = f.read()
 
 setup(
     name='gracula',
     packages=['gracula'],
-    version='1.0.0',
+    version='1.0.3',
     license='MIT',
-    install_requires=[],
     author='yutakojima',
     author_email='info@idea-tools.space',
-    url='',
-    description='very simple twitter api. This package can be done without OAuth.',
+    url='https://github.com/yuutakojima/gracula',
+    description='Gracula is a very simple Twitter api. This package can be done without OAuth.',
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     keywords='python twitter api',
     classifiers=[
         'License :: OSI Approved :: MIT License',
