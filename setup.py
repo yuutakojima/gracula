900e57a9ad03d569b77269f5f18f23adda582837
yu <>
2020-08-06 22:03:51 +0900

    fix setup config

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
-import setuptools
+from setuptools import setup
+from codecs import open
+from os import path
 
-setuptools.setup(
-    name="gracula",
-    version="1.0",
-    author="yutakojima",
-    author_email="info@idea-tools.space",
-    description="very simple twitter api",
-    long_description="very simple twitter api. This package can be done without OAuth.",
-    long_description_content_type="text/markdown",
-    url="",
-    packages=setuptools.find_packages(),
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='gracula',
+    packages=['gracula'],
+    version='1.0.0',
+    license='MIT',
+    install_requires=[],
+    author='yutakojima',
+    author_email='info@idea-tools.space',
+    url='',
+    description='very simple twitter api. This package can be done without OAuth.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='python twitter api',
     classifiers=[
-        "Programming Language :: Python :: 3.6.5",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
